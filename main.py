fc5edde066920a71aaf049c55ed9b92bf793c4fa
Voja <lazarlaz456@gmail.com>
2026-02-27 23:34:20 +0100

    Added show forecast button

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import requests as r
+from tkinter import ttk
+import customtkinter as ctk
 
 class Weather_App(tk.Tk):
     #Initialize the window
@@ -7,8 +10,12 @@ class Weather_App(tk.Tk):
         self.title("Welcome to the weather app")
         self.config(padx=3, pady=3)
 
-        self.canvas = tk.Canvas(self, bg="#D3D3D3", height=500, width=500)
+        self.canvas = tk.Canvas(self, bg="#bcd3d4", height=500, width=500)
         self.canvas.grid(row=0, column=0, rowspan=6, columnspan=5, sticky="nsew")
+        
+        self.get_weather_data_button = ctk.CTkButton(self, text="Show Forecast", fg_color="#817fa0", hover_color="#9A98B8", text_color="#2B2B2F", 
+                                                     font=("Times New Roman", 20, "italic"), border_width=1, border_color="#2B2B2F", anchor="center")
+        self.get_weather_data_button.grid(row=0, column=4)
 
 app = Weather_App()
 app.mainloop()
